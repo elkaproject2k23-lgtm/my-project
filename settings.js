a97e97d54154f30055e4f03dfd00e31e0fcdd295
elkaproject2k23-lgtm <elkaproject2k23@gmail.com>
2026-04-18 13:53:37 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,2 +1,24 @@
-module.exports = {};
-
+module.exports = {
+    uiPort: process.env.PORT || 1880,
+    
+    userDir: '/tmp/node-red-data',
+    nodesDir: '/tmp/node-red-data/nodes',
+    
+    flowFile: 'flows.json',
+    flowFilePretty: true,
+    
+    
+    editorTheme: {
+        projects: {
+            enabled: false
+        }
+    },
+    
+    logging: {
+        console: {
+            level: "info",
+            metrics: false,
+            audit: false
+        }
+    }
+}
